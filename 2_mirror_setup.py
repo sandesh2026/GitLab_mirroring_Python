deb879b96be74b3fae7f744a7fa2bba087b72b9c
rohitnb <rohitnb@github.com>
2021-01-26 10:58:29 +0530

    Mirror Checks

--- 2_mirror_setup.py
+++ 2_mirror_setup.py
@@ -61,6 +61,24 @@ def create_github_repo(github_access_token,mirror_object):
     else:
         return False
 
+def does_gitlab_mirror_exist(gitlab_access_token,mirror_object,github_username):
+    url = "https://gitlab.com/api/v4/projects/"+mirror_object['GitLab Project ID']+"/remote_mirrors"
+    payload={}
+    headers = {
+        'Private-Token': gitlab_access_token,
+    }
+    github_repo = mirror_object['GitHub Org']+"/"+mirror_object['GitLab Project Name']+".git"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    if response.status_code == 200:
+        response_dict = json.loads(response.text)
+        for i in response_dict:
+            if github_repo.lower() in i['url'].lower() and i['enabled']:
+                return True
+        else:
+            return False
+    else:
+        print("DEBUG: Error getting remote mirror details"+str)
+
 creds = read_csv_file("tokens.csv").pop(0)
 gitlab_access_token = creds['GitLab Access Token']
 github_access_token = creds['GitHub Access Token']
@@ -69,10 +87,13 @@ data_objects=read_csv_file("data_file.csv")
 for i in data_objects:
     if does_github_repo_exist(github_access_token,i['GitHub Org'],i['GitLab Project Name']):
         i['GitHub exists'] = True
-        print("Repo Exists")
-        add_remote(github_access_token,gitlab_access_token,i,github_username)
+        if not does_gitlab_mirror_exist(gitlab_access_token,i,github_username):
+            add_remote(github_access_token,gitlab_access_token,i,github_username)
+        else:
+            print("Mapping of GitLab: "+str(i['GitLab Group Name'])+"/"+str(i['GitLab Project Name'])+"---> GitHub:"+str(i['GitHub Org'])+"/"+str(i['GitLab Project Name'])+" already exists! Skipping this!")
     else:
         i['GitHub exists'] = False
-        print("Repo doesn't exist")
-        create_github_repo(github_access_token,i)
-        add_remote(github_access_token,i['GitHub Org'],i,github_username)
+        if create_github_repo(github_access_token,i):
+            add_remote(github_access_token,i['GitHub Org'],i,github_username)
+        else:
+            print("DEBUG: Error in creating the repo. Check the data_file.csv")
